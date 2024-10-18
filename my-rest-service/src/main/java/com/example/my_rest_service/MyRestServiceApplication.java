25ead6abb1aea3b0c56ade55fad19021fc2db000
HanziEDU <miki.handzlikpl@gmail.com>
2024-10-18 13:59:50 +0000

    Testowe

---
+++ my-rest-service/src/main/java/com/example/my_rest_service/MyRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestServiceApplication.class, args);
+	}
+
+}
